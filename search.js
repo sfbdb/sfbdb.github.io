1a5b1395fc2cc1c07a6e73107386a2b0faed4df0
Jimmy Praet <jimmy.praet@telenet.be>
2020-04-22 20:28:30 +0200

    improved filtering

--- search.js
+++ search.js
@@ -1,28 +1,34 @@
 var template = Handlebars.compile(document.getElementById('sfb-cards-template').innerHTML);
 var urlParams = new URLSearchParams(window.location.search);
-var tag = urlParams.get('tag');
-var type = urlParams.get('type');
-var core = urlParams.get('core');
-var target = urlParams.get('target');
-var movement = urlParams.get('movement');
+var filters = {
+  "tag": urlParams.get('tag'),
+  "type": urlParams.get('type'),
+  "core": urlParams.get('core'),
+  "target": urlParams.get('target'),
+  "movement": urlParams.get('movement'),
+  "champion": urlParams.get('champion'),
+}
 
 var cards = [];
-data.champions.forEach((champion) => {
-  champion.cards.forEach((card, i) => {
-    if ((!tag || card.tags.includes(tag))
-        && (!type || card.type == type)
-        && (!core || card.core == core) 
-        && (!target || card.target == target) 
-        && (!movement || card.movement)) {
-      cards.push({
-        "card": card,
-        "index": i + 1,
-        "champion_name": champion.name,
-        "champion_code": champion.code
-      });
-    }
-  });
+data.champions.forEach((champ) => {
+  if (!filters.champion || champ.code == filters.champion) {
+    champ.cards.forEach((card, i) => {
+      if ((!filters.tag || card.tags.includes(filters.tag))
+          && (!filters.type || card.type == filters.type)
+          && (!filters.core || card.core == filters.core) 
+          && (!filters.target || card.target == filters.target) 
+          && (!filters.movement || card.movement)) {
+        cards.push({
+          "card": card,
+          "index": i + 1,
+          "champion_name": champ.name,
+          "champion_code": champ.code
+        });
+      }
+    });    
+  }
 });
+
 Handlebars.registerHelper('ifEq', function(lvalue, rvalue, options) {
   if (lvalue != rvalue) {
     return options.inverse(this);
@@ -30,12 +36,18 @@ Handlebars.registerHelper('ifEq', function(lvalue, rvalue, options) {
     return options.fn(this);
   }
 });
-var html = template(cards);
+var html = template({"params": urlParams.toString(), "filters": filters, "cards": cards});
 document.getElementById('content').innerHTML = html;
 
 function search(key, value) {
   var urlParams = new URLSearchParams(window.location.search);
   urlParams.delete(key);
   urlParams.append(key, value);
+  window.location.search = urlParams;
+}
+
+function removeSearch(key, value) {
+  var urlParams = new URLSearchParams(window.location.search);
+  urlParams.delete(key);
   window.location.search = urlParams;
 }
